aa3d8ddda4eaca3674039b99094d5a8a93bb8040
Jessica F. Martinez <jessicafndez@gmail.com>
2025-06-13 15:11:39 +0200

    update results table

--- src/lib/constants.js
+++ src/lib/constants.js
@@ -24,22 +24,58 @@ export const BEACON_NETWORK_COLUMNS = [
     width: '10%'
   },
   {
-    id: 'detail',
-    label: "",
+    id: 'contact',
+    label: "Contact",
+    width: '10%',
+    align: 'right'
+  }
+];
+
+export const BEACON_SINGLE_COLUMNS = [
+  { 
+    id: 'beacon_dataset',
+    label: "Dataset",
+    align: 'left',
+    width: '30%'
+  },
+  {
+    id: 'maturity',
+    label: "Beacon Maturity",
+    align: 'center',
+    width: '20%'
+  },
+  {
+    id: 'datasets_count',
+    label: "nº of Datasets",
+    align: 'right',
+    numeric: true,
+    width: '20%'
+  },
+  {
+    id: 'response',
+    label: "Response",
+    width: '10%'
+  },
+  {
+    id: 'details',
+    label: "Details",
     width: '10%'
   },
   {
     id: 'contact',
     label: "Contact",
-    width: '10%'
+    width: '10%',
+    align: 'right'
   }
 ];
 
 export const BEACON_NETWORK_COLUMNS_EXPANDED = {
   'beacon_dataset_name': '70%',
   'beacon_dataset_response': '10%',
-  'beacon_dataset_detail': '10%',
-  'beacon_dataset_contact': '10%'
+  'beacon_dataset_detail': {
+    width: '20%',
+    float: 'right'
+  }
 }
 
 export const DATASET_TABLE = [
